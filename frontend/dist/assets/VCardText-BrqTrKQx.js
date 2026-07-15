464379fe2ae3b84f8d12e695aa6fcf711dda7459
daniel <mendezamutarid@gmail.com>
2026-07-15 15:53:04 -0400

    src del ultimo commit

---
+++ frontend/dist/assets/VCardText-BrqTrKQx.js
@@ -0,0 +1,1 @@
+import{a$ as t}from"./index-CAW5TODy.js";const e=t("v-card-text");export{e as V};
---
+++ frontend/dist/assets/VSpacer-CF_udhT6.js
@@ -0,0 +1,1 @@
+import{a$ as a}from"./index-CAW5TODy.js";const r=a("v-spacer","div","VSpacer");export{r as V};
